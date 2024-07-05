dcb063dd0045de0cfb59d02103bd92cd72ecf36a
Thanveer Sherief - vn55jeb <thanveer.sherief@walmart.com>
2024-07-05 17:42:49 +0530

    Terms and Privacy page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,47 @@
 import './App.css'
 import NavBar from './components/NavBar.jsx';
 import HomePage from './pages/Home.jsx';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
 import ContactForm from './pages/ContactForm.jsx';
 import About from './pages/About.jsx';
 import Footer from './components/Footer.jsx';
+import PrivacyPolicy from './pages/PrivacyPolicy.jsx';
+import TermsAndCondition from './pages/TermsAndCondition.jsx';
 
-function App() {
+const App = () => {
+  return (
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
+  );
+};
+
+const AppContent = () => {
+  const location = useLocation();
+
+  // Define paths where NavBar and Footer should be hidden
+  const hideHeaderFooterPaths = ['/privacyPolicy', '/terms'];
+
+  // Function to check if current path should hide NavBar and Footer
+  const shouldHideHeaderFooter = () => {
+    return hideHeaderFooterPaths.includes(location.pathname);
+  };
 
   return (
     <>
-    <div className="App">
-      <BrowserRouter>
-      <NavBar />
+      {!shouldHideHeaderFooter() && <NavBar />}
 
       <Routes>
-        <Route path='/' element ={<HomePage />}/>
-        <Route path='/contact' element={<ContactForm/>}/>
-        <Route path='/about' element={<About/>}/>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/contact' element={<ContactForm />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/privacyPolicy' element={<PrivacyPolicy />} />
+        <Route path='/terms' element={<TermsAndCondition />} />
       </Routes>
-      <Footer/>
 
-      </BrowserRouter>
-    </div>
-      </>
-  )
-}
+      {!shouldHideHeaderFooter() && <Footer />}
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/TermsAndCondition.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const TermsAndCondition = () => {
+    return (
+        <div className="container mx-auto py-8 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold mb-4">Terms and Conditions</h1>
+          <p className="mb-4">
+            <strong>Acceptance of Terms:</strong> By using the PettoBazaar mobile application (App), you agree to be bound by these Terms and Conditions, our Privacy Policy, and all applicable laws and regulations. If you do not agree with any of these terms, you are prohibited from using or accessing this app.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">Use License:</h2>
+          <p className="mb-4">
+            Permission is granted to download a copy of the app on a mobile device solely for personal, non-commercial use. This is the grant of a license, not a transfer of title, and under this license, you may not:
+          </p>
+          <ul className="list-disc ml-8 mb-4">
+            <li>Modify or copy the materials.</li>
+            <li>Use the materials for any commercial purpose, or for any public display (commercial or non-commercial).</li>
+            <li>Attempt to decompile or reverse engineer any software contained on the app.</li>
+            <li>Remove any copyright or other proprietary notations from the materials.</li>
+          </ul>
+    
+          <h2 className="text-2xl font-bold mb-2">User Account and Security:</h2>
+          <p className="mb-4">
+            Users are responsible for maintaining the confidentiality of their account and password information. PettoBazaar reserves the right to refuse service, terminate accounts, remove or edit content in its sole discretion.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">User-Generated Content and Moderation:</h2>
+          <p className="mb-4">
+            <strong>Content Moderation:</strong> PettoBazaar takes measures to moderate user-generated content to prevent objectionable or abusive behavior.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">Terms of Use:</h2>
+          <p className="mb-4">
+            By using PettoBazaar, users agree to abide by the terms of use and community guidelines. Users must understand that any objectionable content or abusive behavior will not be tolerated.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">Report Abuse:</h2>
+          <p className="mb-4">
+            Users are encouraged to report any objectionable content or abusive behavior encountered on PettoBazaar. Reports will be reviewed promptly, and appropriate action will be taken.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">Zero Tolerance Policy:</h2>
+          <p className="mb-4">
+            PettoBazaar has a zero-tolerance policy towards objectionable content and abusive users. Any violation of the terms of use will result in immediate account suspension or termination.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">User Content:</h2>
+          <p className="mb-4">
+            By posting or submitting any content on PettoBazaar, Users grant PettoBazaar a non-exclusive, royalty-free, perpetual, and irrevocable right to use, reproduce, modify, adapt, publish, translate, distribute, and display such content.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">Limitations:</h2>
+          <p className="mb-4">
+            In no event shall PettoBazaar be liable for any damages arising out of the use or inability to use the app, even if PettoBazaar has been notified orally or in writing of the possibility of such damage.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">Modifications:</h2>
+          <p className="mb-4">
+            PettoBazaar may revise these terms of use at any time without notice. By using this app, you agree to be bound by the current version of these Terms and Conditions.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">Governing Law:</h2>
+          <p className="mb-4">
+            These terms and conditions are governed by and construed in accordance with the laws of India, and you irrevocably submit to the exclusive jurisdiction of the courts in that location.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">Severability:</h2>
+          <p className="mb-4">
+            If any provision of these terms is deemed unlawful, void, or for any reason unenforceable, then that provision shall be deemed severable from these terms and shall not affect the validity and enforceability of any remaining provisions.
+          </p>
+    
+          <h2 className="text-2xl font-bold mb-2">Contact Us:</h2>
+          <p className="mb-4">
+            If you have any questions about these Terms and Conditions, please contact us at <a href="mailto:pettobazzar@gmail.com" className="text-blue-500">info.pettobazaar@gmail.com</a>.
+          </p>
+    
+          <p className="mb-4">
+            <strong>END USER LICENSE AGREEMENT</strong> Last updated July 5th, 2024
+          </p>
+        </div>
+      );
+}
+
+export default TermsAndCondition
